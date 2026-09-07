2997ac898f08e6969ed6258da938dc8b2425ce53
Sinkandika <andika.alstam@gmail.com>
2026-09-07 12:06:45 +0700

    Prepare frontend for vercel

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // modify HTTP requests and responses globally before they are handled
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || import.meta.env.VITE_API_LOCAL_URL,
 });
 
 axiosInstance.interceptors.request.use(
